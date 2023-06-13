f7f6f2a0609bed082e88122dc126ab90a3cccfeb
Fahim Shariar <fahimshariar28@gmail.com>
2023-06-14 00:42:06 +0600

    Initial Setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Task Manager in running!");
+});
+
+app.listen(port, () => {
+  console.log(`Task Manager Server is running on port: ${port}`);
+});
