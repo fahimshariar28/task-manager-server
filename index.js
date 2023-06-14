5ec63dee23b545c8dd839fb06efc2e0dcfe10156
Fahim Shariar <fahimshariar28@gmail.com>
2023-06-14 14:58:23 +0600

    delete task

--- index.js
+++ index.js
@@ -98,6 +98,14 @@ async function run() {
       res.send(result);
     });
 
+    // Delete task
+    app.delete("/deleteTask", verifyJWT, async (req, res) => {
+      const id = req.query.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
